3145f3ff58b766d76632422928115e10f02bb016
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-09 20:49:45 -0500

    first commit

---
+++ src/main/java/com/Project/ChickenDeliveryBack/ChickenDeliveryBackApplication.java
@@ -0,0 +1,13 @@
+package com.Project.ChickenDeliveryBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenDeliveryBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenDeliveryBackApplication.class, args);
+	}
+
+}
