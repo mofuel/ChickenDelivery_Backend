ffd5b5f440ca2c7219c9c8b15d7358a5228e03f1
Miguel Monzon <miguel.monzon.lucio@gmail.com>
2026-06-22 20:36:57 -0500

    Add DTO ProductoDTO

---
+++ src/main/java/com/Project/ChickenDeliveryBack/domain/dto/ProductDTO.java
@@ -0,0 +1,28 @@
+package com.Project.ChickenDeliveryBack.domain.dto;
+
+public class ProductDTO {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private Boolean estado;
+    private Integer idCategoria;
+    private String nombreCategoria;
+
+    public ProductDTO() {}
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public Double getPrecio() { return precio; }
+    public void setPrecio(Double precio) { this.precio = precio; }
+    public Boolean getEstado() { return estado; }
+    public void setEstado(Boolean estado) { this.estado = estado; }
+    public Integer getIdCategoria() { return idCategoria; }
+    public void setIdCategoria(Integer idCategoria) { this.idCategoria = idCategoria; }
+    public String getNombreCategoria() { return nombreCategoria; }
+    public void setNombreCategoria(String nombreCategoria) { this.nombreCategoria = nombreCategoria; }
+}
